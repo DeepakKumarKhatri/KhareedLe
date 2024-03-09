c8c196f15e3a7ef888b25fc70e0c3182325471e4
DeepakKumarKhatri <SP21-BCS-017@ISBSTUDENT.COMSATS.EDU.PK>
2024-03-09 12:48:21 +0500

    Added Login & SignUp Pages

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,12 +4,15 @@ import {Counter} from './features/counter/Counter';
 import ProductList from "./features/product-list/ProductList";
 import Navbar from "./features/navbar/Navbar";
 import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/LoginPage";
+import SignUpPage from "./pages/SignUpPage";
 
 function App() {
   return (
     <div className="App">
       {/* <Navbar/> */}
-      <HomePage/>
+      {/* <HomePage/> */}
+      <SignUpPage/>
       {/* <ProductList/> */}
     </div>
   );
--- frontend/src/features/product-list/ProductList.js
+++ frontend/src/features/product-list/ProductList.js
@@ -472,6 +472,13 @@ const ProductList = () => {
                   >
                     2
                   </a>
+                  <a
+                    href="#"
+                    className="relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0"
+                  >
+                    <span className="sr-only">Next</span>
+                    <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+                  </a>
                 </nav>
               </div>
             </div>
